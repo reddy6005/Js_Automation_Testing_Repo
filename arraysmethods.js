0cf30493e95a85b33b91f75e046f17fb20157ffa
Bhargav Reddy <bhargav.v@caw.tech>
2026-02-21 22:54:11 +0530

    added some more lines of code

---
+++ arraysmethods.js
@@ -0,0 +1,9 @@
+let cars=["volovo","bmw","audi","mercedes"]
+console.log(cars.length) //length of the array
+console.log(cars)
+
+console.log(cars.toString()) //converts array to string
+
+//pop() method removes the last element from an array and returns that element. This method changes the length of the array.'
+console.log(cars.pop()) //removes the last element of the array and returns it
+console.log(cars)
